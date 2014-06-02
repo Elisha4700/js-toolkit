5637b7deeda68d20990a78fa8822fb8755d90938
Elisha4700 <elishas@sinapsa.co.il>
2014-06-02 21:51:11 +0300

    made route to work

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,36 @@
     'use strict';
 
     var app = angular.module('App', [
+        'ngRoute',
         'Controllers'
         ]);
 
 
-    // app.config(function ($routeProvider) {
-    //     $routeProvider
-
-    // });
-
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/yeoman',
+            {
+                templateUrl: 'yeoman/yeoman.html'
+            })
+            .when('/bower',
+            {
+                templateUrl: 'bower/bower.html'
+            })
+            .when('/grunt',
+            {
+                templateUrl: 'grunt/grunt.html'
+            })
+            .when('/gulp',
+            {
+                templateUrl: 'gulp/gulp.html'
+            })
+            .when('/karma',
+            {
+                templateUrl: 'karma/karma.html'
+            })
+            .otherwise({
+                redirectTo: '/yeoman'
+            });
+    });
 
 })(angular);
